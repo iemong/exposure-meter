f2b0465d0ffda09e6fab2964a4ced4f99f5be8b5
iemong <kiyoshidainagon@gmail.com>
2022-08-15 23:39:23 +0900

    大体できた

--- main.py
+++ main.py
@@ -29,6 +29,16 @@ label2 = M5TextBox(45, 38, "label2", lcd.FONT_DejaVu18, 0xff0000, rotate=0)
 label3 = M5TextBox(0, 70, "Bit Shift:", lcd.FONT_DejaVu18, 0xff0000, rotate=0)
 label4 = M5TextBox(0, 90, "EV:", lcd.FONT_DejaVu18, 0xff0000, rotate=0)
 label5 = M5TextBox(45, 90, "label4:", lcd.FONT_DejaVu18, 0xff0000, rotate=0)
+label6 = M5TextBox(0, 120, "F2:", lcd.FONT_DejaVu18, 0xff0000, rotate=0)
+label7 = M5TextBox(0, 140, "F4:", lcd.FONT_DejaVu18, 0xff0000, rotate=0)
+label8 = M5TextBox(0, 160, "F8:", lcd.FONT_DejaVu18, 0xff0000, rotate=0)
+label9 = M5TextBox(0, 180, "F11:", lcd.FONT_DejaVu18, 0xff0000, rotate=0)
+
+f_list = [2, 4, 8, 11]
+
+
+def calcSS(ev):
+    return lambda av: math.floor(1 / (2 ** (-1 * (ev - 2 * math.log2(av)))))
 
 
 def buttonA_wasPressed():
@@ -45,9 +55,16 @@ btnA.wasPressed(buttonA_wasPressed)
 
 incr = 1
 hat_dlight_0.set_mode(0x11)
+iso = 400
 while True:
     lux = hat_dlight_0.get_lux()
+    ev = convertLux2Ev(lux, iso)
+    tv_list = list(map(calcSS(ev), f_list))
     label2.setText(str(lux))
-    label5.setText(str(convertLux2Ev(lux, 400)))
+    label5.setText(str(ev))
+    label6.setText(str(tv_list[0]))
+    label7.setText(str(tv_list[1]))
+    label8.setText(str(tv_list[2]))
+    label9.setText(str(tv_list[3]))
     wait_ms(150)
     wait_ms(2)
